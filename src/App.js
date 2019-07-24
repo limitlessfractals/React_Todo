eb72783b3a0f3b7644649c3a84f6673581899920
limitlessfractals <qwork@Wuqiongs-MacBook-Pro.local>
2019-07-23 20:02:32 -0700

    markComplete, delTodo, Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
 
 class App extends Component{
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting',
+        completed: false
+      }
+    ]
+  }
+
+  //toggles state at top, which is brought down through the components
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed 
+      }
+      return todo;
+    }) });
+  }
+
+  // Delete Todo; ... is spread operator; setState to items that don't match id
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  }
+
   render() {
+    //console.log(this.state.todos)
     return(
       <div className = "App">
-        <h1>App</h1>
+        <Header />
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+        />
       </div>
+      // markComplete is a prop from Todos.js
     );
   }
 }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,13 +1,27 @@
+//rce tab
+
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
 class Todos extends Component{
   render() {
-    return(
-      <div>
-        <h1>Todos</h1>
-      </div>
-    );
+    //console.log(this.props.todos)
+    return this.props.todos.map((todo) => (
+      <TodoItem
+        key={todo.id} 
+        todo={todo} 
+        markComplete={this.props.markComplete} 
+        delTodo={this.props.delTodo}
+      />  //todo is a prop from TodoItem.js; markComplete is a prop in App.js
+    ));
   }
+}
+
+// PropTypes
+// todos is array; todo is object
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
 }
 
 export default Todos;
